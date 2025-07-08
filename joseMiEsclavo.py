8ac1f60a3b9ec857317dc755035097b5a8d6e714
Daniel Valarezo <danishovala@gmail.com>
2025-07-08 10:55:51 -0500

    ia para tu amigo que se llame jose

---
+++ joseMiEsclavo.py
@@ -0,0 +1,24 @@
+ily = input("preguntale algo a jose:")
+while ily == "eso es todo":
+   if ily == "hola":
+       print("Hola mucho gusto en hablar contigo ¿en que puedo ayudarte?")
+   elif ily == "hora":
+       print("actualmente no se eso pero puedes direigirte al humano mas cercano y preguntarle la hora")
+   elif ily == "operacion":
+       print("introduce los digitos de la operacion")
+       int = input("digito1:")
+       int2 = input("digito2:")
+       b = input("operacion:")
+       if b == "suma":
+           print(int + int2)
+       elif b == "resta":
+           print(int - int2)
+       elif b == "multiplicacion":
+           print(int * int2)
+       elif b == "division":
+           print(int / int2)
+           
+              
+         
+
+   
